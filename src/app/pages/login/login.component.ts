e6542b54d8c35e5b38fb28f460b5ff46aa075008
Abdoulaye Aliou Sall <gallebaba71@gmail.com>
2024-05-11 10:23:16 +0000

    personnaliser le login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  loginForm: FormGroup = new FormGroup({
+    username: new FormControl('',[Validators.required,Validators.email]),
+    motDePasse:new FormControl('',[Validators.required,Validators.minLength(4)])
+  })
+  ngOnInit(): void {
+  }
+
+
+  constructor(){
+
+  }
+
+
+  login(){
+    console.log(this.loginForm.value);
+    
+  }
 
 }
